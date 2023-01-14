4342787d8262d4367133fb0046c4586b6ec633d6
DiegoFS <dfs131294@gmail.com>
2023-01-13 23:40:59 -0500

    application restart endpoint added

--- src/main/java/com/diego/securityflows/SecurityFlowsApplication.java
+++ src/main/java/com/diego/securityflows/SecurityFlowsApplication.java
@@ -1,19 +1,34 @@
 package com.diego.securityflows;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SecurityFlowsApplication {
+	private static ConfigurableApplicationContext context;
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityFlowsApplication.class, args);
+		context = SpringApplication.run(SecurityFlowsApplication.class, args);
 	}
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	public static void restart() {
+		ApplicationArguments args = context.getBean(ApplicationArguments.class);
+
+		Thread thread = new Thread(() -> {
+			context.close();
+			context = SpringApplication.run(SecurityFlowsApplication.class, args.getSourceArgs());
+		});
+
+		thread.setDaemon(false);
+		thread.start();
 	}
 }
--- src/main/java/com/diego/securityflows/controller/HomeController.java
+++ src/main/java/com/diego/securityflows/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.diego.securityflows.controller;
 
+import com.diego.securityflows.SecurityFlowsApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +16,10 @@ public class HomeController {
     @GetMapping("external/hello")
     public String basicAuthHello() {
         return "Hello... Basic Authentication!!";
+    }
+
+    @PostMapping("/restart")
+    public void restart() {
+        SecurityFlowsApplication.restart();
     }
 }
